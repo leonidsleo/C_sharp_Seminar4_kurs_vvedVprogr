97ad5fbfda4b8e1af1ca8ffe951891646a44ea26
leonid <leonidsleo@yandex.ru>
2023-05-18 20:18:51 +0300

    сохранено решение зад 28, семинар 4 курс введ. в програм

--- C#_Seminar4_Zad28/Program.cs
+++ C#_Seminar4_Zad28/Program.cs
@@ -2,16 +2,41 @@
 // 4 -> 24 
 // 5 -> 120
 
+// Console.Write("Введите число num = ");
+// long num = Convert.ToInt64(Console.ReadLine());
+// long work = 1;
+// long i = 1;
+// // for (long i = 1; i <= num; i++)
+// // {
+// //     work = work * i;
+// // }
+// // Console.WriteLine($"Произведение от 1 до числа {num} равно {work}");
+// if (num <= 20)
+// {
+//     while (i <= num)
+//     {
+//         work = work * i;
+//         i++;
+//     }
+//     Console.WriteLine($"Произведение от 1 до числа {num} равно {work}");
+// }
+// else
+// {
+//     Console.WriteLine($"Произведение всех чисел до {num} слишком велико. Введите число менее 21");
+// }
+
 Console.Write("Введите число num = ");
 long num = Convert.ToInt64(Console.ReadLine());
 long work = 1;
-if (num > 55)
+if (num <= 20)
 {
-    Console.WriteLine($"Произведение всех чисел до {num} слишком велико. Введите число менее 55");
+    for (long i = 1; i <= num; i++)
+    {
+        work = work * i;
+    }
+    Console.WriteLine($"Произведение от 1 до числа {num} равно {work}");
 }
 else
-for (long i = 1; i <= num; i++)
 {
-    work = work * i;
+    Console.WriteLine($"Произведение всех чисел до {num} слишком велико. Введите число менее 21");
 }
-Console.WriteLine($"Произведение от 1 до числа {num} равно {work}");
