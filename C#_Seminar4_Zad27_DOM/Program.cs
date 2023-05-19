b0a114183f3bf18f4b7b2c4d702e048effd93b74
leonid <leonidsleo@yandex.ru>
2023-05-19 21:54:30 +0300

    курс-введ в прогр, семинар 4, дом работа, задача 27 решена 1-м способом

---
+++ C#_Seminar4_Zad27_DOM/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.WriteLine("Сумма всех цифр введенного числа!");
+Console.Write("Введите число - ");
+int num = Convert.ToInt32(Console.ReadLine());
+int size = (int)Math.Log10(num) + 1;
+//Console.WriteLine($"Длинна {size}");
+int summ = 0;
+for (int i = 1; i <= size; i++)
+{
+    int num2 = num % 10;
+    summ = summ + num2;
+    num = num / 10;
+}
+Console.WriteLine($"Сумма {summ}");
