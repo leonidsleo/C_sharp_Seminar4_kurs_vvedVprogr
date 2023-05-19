e765377a6856557b8689e74c1b21083e096edd8d
leonid <leonidsleo@yandex.ru>
2023-05-19 22:51:07 +0300

    курс: введ в прогр, семинар4, задача 24: добавил второе решение

--- C#_Seminar4_Zad24/Program.cs
+++ C#_Seminar4_Zad24/Program.cs
@@ -3,11 +3,23 @@
 // 4 -> 10
 // 8 -> 36
 
-Console.Write("Введите число = ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число = ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int summ = 0;
+// for (int i = 1; i <= num; i++)
+// {
+//     summ = summ + i;
+//     }
+// Console.WriteLine($"Сумма чисел = {summ}");
+
+// ВАРИАНТ 2:
+int num = Convert.ToInt32(new Random().Next(0, 99));
+Console.WriteLine($"Нахождение суммы от 1 до числа {num};");
 int summ = 0;
-for (int i = 1; i <= num; i++)
+int num2 = 1;
+while (num2 <= num)
 {
-    summ = summ + i;
-    }
+    summ = summ + num2;
+    num2 = num2 +1;
+}
 Console.WriteLine($"Сумма чисел = {summ}");
