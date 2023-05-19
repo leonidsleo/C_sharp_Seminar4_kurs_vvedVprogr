5b16873d55c9fcd85b16fa436981c35e23607164
leonid <leonidsleo@yandex.ru>
2023-05-19 22:29:25 +0300

    введ в прог, семинар4, домРабота, решил задачу 29

---
+++ C#_Seminar4_Zad29_DOM/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int size = 8;
+Console.WriteLine($"Массив из {size} элементов;");
+int[] massif = new int[size];
+for (int i = 0; i < size; i++)
+{
+    massif[i] = new Random().Next(-999, 999);
+}
+Console.WriteLine("Massif [{0}]", string.Join(", ", massif));
